14ba4b77a7e9ce5678e70013ccd61cf6f8dfe593
Deryn <62044922+iFishae@users.noreply.github.com>
2022-03-22 18:43:13 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,55 @@
+/** @format */
+
+console.clear();
+
+const Hypixel = require('hypixel-api-reborn');
+
+const Client = require("./Structures/Client.js");
+
+const Command = require("./Structures/Command.js");
+
+const config = require("./Data/config.json");
+
+const client = new Client();
+
+const Discord = require('discord.js');
+
+const hypixel = new Hypixel.Client('INSERT-YOUR-HYPIXEL-API-KEY-HERE');
+
+exports.hypixel = hypixel
+
+const fs = require("fs");
+
+fs.readdirSync("./src/Commands")
+	.filter(file => file.endsWith(".js"))
+	.forEach(file => {
+		/**
+		 * @type {Command}
+		 */
+		const command = require(`./Commands/${file}`);
+		console.log(`Command ${command.name} loaded`);
+		client.commands.set(command.name, command);
+	});
+
+client.on("ready", () => console.log(`${client.user.tag} is online.`));
+
+client.on("messageCreate", message => {
+	if (message.author.bot) return;
+
+	if (!message.content.startsWith(config.prefix)) return;
+
+	const args = message.content.substring(config.prefix.length).split(/ +/);
+
+	const command = client.commands.find(cmd => cmd.name == args[0]);
+
+	const invalidcommandEmbed = new Discord.MessageEmbed()
+		.setColor('#000000')
+		.setTitle('Invalid Command!')
+		.setDescription('Please do >help to use find the correct command usage!')
+
+	if (!command) return message.reply({ embeds: [invalidcommandEmbed] });
+
+	command.run(message, args, client);
+});
+
+client.login(config.token);
