a38c0d59e943afd429ec3ceff816247c4ffd75ab
Deryn <62044922+iFishae@users.noreply.github.com>
2022-03-22 19:31:23 -0400

    added a set status

---
+++ src/Commands/setstatus.js
@@ -0,0 +1,29 @@
+/** @format */
+
+const Command = require("../Structures/Command.js");
+const Hypixel = require('hypixel-api-reborn');
+const index = require(`../index.js`);
+const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+const hypixel = index.hypixel
+
+module.exports = new Command({
+  name: "status",
+  description: "sets bot status",
+
+  async run(message, args, client) {
+    if (message.author.id !== '698331475359170640') return message.reply(`You are not Coopsie! You cannot use this command!`)
+    message.channel.send(`Status Updated`)
+    const arrayOfStatus = [
+        `with kids in my cellar.`,
+      ];
+  
+      let index = 0;
+      setInterval(() => {
+        if (index === arrayOfStatus.length) index = 0;
+        const status = arrayOfStatus[index];
+        client.user.setActivity(status, { type: 'PLAYING' });
+        index++;
+      }, 300000)
+    }
+  })
